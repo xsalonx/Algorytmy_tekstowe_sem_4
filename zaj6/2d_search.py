da9e9d7bfa03d7f2807a6f4ba0ee7a94c0beded2
xsalonx <lukasz.dubiel3@gmail.com>
2021-05-28 12:34:06 +0200

    zaj6_done

--- zaj6/2d_search.py
+++ zaj6/2d_search.py
@@ -2,9 +2,11 @@ from aho_corasic import *
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 def search_2d_pattern(text: str,
-                      patterns, FIRST_CHAR=48):  # patterns - list of 2d patterns; each given as "<line1>\n<line2>\n<line3>..."
+                      patterns,
+                      FIRST_CHAR=48):  # patterns - list of 2d patterns; each given as "<line1>\n<line2>\n<line3>..."
     # TODO not so effective parsing text and patterns ...,  but works
 
     Lines = text.split("\n")
@@ -21,7 +23,8 @@ def search_2d_pattern(text: str,
             search_matrix[j, i] = word_idx
 
     pos_searcher = Searcher()
-    pos_search_patterns = ["".join([chr(FIRST_CHAR + subpatterns_searcher.pattern_idx(p)) for p in pc]) for pc in Patterns]
+    pos_search_patterns = ["".join([chr(FIRST_CHAR + subpatterns_searcher.pattern_idx(p)) for p in pc]) for pc in
+                           Patterns]
     pos_searcher.add_all(pos_search_patterns)
 
     columns_as_text = ["".join([chr(i + FIRST_CHAR) for i in search_matrix[:, c]]) for c in range(shape[1])]
@@ -37,24 +40,34 @@ def search_2d_image_pattern(im_path, pattern_paths, FIRST_CHAR=48):
     image = np.array(Image.open(im_path).convert('L'))
     pattern_ims = [np.array(Image.open(p).convert('L')) for p in pattern_paths]
 
-    image_text = "\n".join(["".join([chr(image[j, i] + FIRST_CHAR) for i in range(image.shape[1])]) for j in range(image.shape[0])])
-    pattern_text = ["\n".join(["".join([chr(p_im[j, i] + FIRST_CHAR) for i in range(p_im.shape[1])]) for j in range(p_im.shape[0])]) for p_im in pattern_ims]
+    image_text = "\n".join(
+        ["".join([chr(image[j, i] + FIRST_CHAR) for i in range(image.shape[1])]) for j in range(image.shape[0])])
+    pattern_text = [
+        "\n".join(["".join([chr(p_im[j, i] + FIRST_CHAR) for i in range(p_im.shape[1])]) for j in range(p_im.shape[0])])
+        for p_im in pattern_ims]
 
     Res, search_matrix = search_2d_pattern(image_text, pattern_text, FIRST_CHAR=FIRST_CHAR)
 
     return Res, search_matrix
 
-def main_im(whole_text_im_file_path, patter_im_file_paths, show_each_ad=False):
 
+def main_im(whole_text_im_file_path, patter_im_file_paths, show_each_ad=False):
     Res, search_matrix = search_2d_image_pattern(whole_text_im_file_path, patter_im_file_paths)
 
     [print(j, r) for j, r in enumerate(Res) if len(r) > 0]
 
     im = np.array(Image.open(whole_text_im_file_path).convert('L'))
     plt.imshow(im)
+
+    Xv = defaultdict(lambda: [], [])
+    Yv = defaultdict(lambda: [], [])
+
     for j, r in enumerate(Res):
         for i, v in r:
-            plt.scatter([i], [j], color="red")
+            Xv[v].append(i)
+            Yv[v].append(j)
+    for v in Xv.keys():
+        plt.scatter(Xv[v], Yv[v])
 
     if show_each_ad:
         X = []
@@ -67,6 +80,7 @@ def main_im(whole_text_im_file_path, patter_im_file_paths, show_each_ad=False):
 
     plt.show()
 
+
 def main_txt(path, patterns):
     with open(path) as f:
         text = f.read()
@@ -75,12 +89,10 @@ def main_txt(path, patterns):
 
 
 if __name__ == '__main__':
-
     text_path = "data/haystack.txt"
     patterns = [f"{chr(i)}\n{chr(i)}" for i in range(97, 123)]
     # main_txt(text_path, patterns)
 
-
     whole_text_im_file_path = "data/haystack_part_1_of_2.png"
-    patters_im_file_paths = ["data/algorithms.png", "data/sequance.png", "data/pattern.png"]
+    patters_im_file_paths = ["data/pattern.png", "data/algorithms.png", "data/of.png"]
     main_im(whole_text_im_file_path, patters_im_file_paths)
