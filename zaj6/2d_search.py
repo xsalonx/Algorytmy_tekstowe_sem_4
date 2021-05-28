63ff1e2abd18b8980b2b493c679d351cb96bf462
xsalonx <lukasz.dubiel3@gmail.com>
2021-05-28 12:17:56 +0200

    zaj6_done

--- zaj6/2d_search.py
+++ zaj6/2d_search.py
@@ -33,20 +33,20 @@ def search_2d_pattern(text: str,
     return Res, search_matrix
 
 
-def search_2d_image_pattern(im_path, pattern_path, FIRST_CHAR=48):
+def search_2d_image_pattern(im_path, pattern_paths, FIRST_CHAR=48):
     image = np.array(Image.open(im_path).convert('L'))
-    pattern_im = np.array(Image.open(pattern_path).convert('L'))
+    pattern_ims = [np.array(Image.open(p).convert('L')) for p in pattern_paths]
 
     image_text = "\n".join(["".join([chr(image[j, i] + FIRST_CHAR) for i in range(image.shape[1])]) for j in range(image.shape[0])])
-    pattern_text = ["\n".join(["".join([chr(pattern_im[j, i] + FIRST_CHAR) for i in range(pattern_im.shape[1])]) for j in range(pattern_im.shape[0])])]
+    pattern_text = ["\n".join(["".join([chr(p_im[j, i] + FIRST_CHAR) for i in range(p_im.shape[1])]) for j in range(p_im.shape[0])]) for p_im in pattern_ims]
 
     Res, search_matrix = search_2d_pattern(image_text, pattern_text, FIRST_CHAR=FIRST_CHAR)
 
     return Res, search_matrix
 
-def main_im(whole_text_im_file_path, patter_im_file, show_each_ad=False):
+def main_im(whole_text_im_file_path, patter_im_file_paths, show_each_ad=False):
 
-    Res, search_matrix = search_2d_image_pattern(whole_text_im_file_path, patter_im_file)
+    Res, search_matrix = search_2d_image_pattern(whole_text_im_file_path, patter_im_file_paths)
 
     [print(j, r) for j, r in enumerate(Res) if len(r) > 0]
 
@@ -64,6 +64,7 @@ def main_im(whole_text_im_file_path, patter_im_file, show_each_ad=False):
                 X.append(idx[1])
                 Y.append(idx[0])
         plt.scatter(X, Y, s=5, color="green")
+
     plt.show()
 
 def main_txt(path, patterns):
@@ -81,5 +82,5 @@ if __name__ == '__main__':
 
 
     whole_text_im_file_path = "data/haystack_part_1_of_2.png"
-    patter_im_file = "data/of.png"
-    main_im(whole_text_im_file_path, patter_im_file)
+    patters_im_file_paths = ["data/algorithms.png", "data/sequance.png", "data/pattern.png"]
+    main_im(whole_text_im_file_path, patters_im_file_paths)
